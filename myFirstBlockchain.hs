84d888605677f02994cf7acf05cc8075be4df9e4
johnny5cfw <john@cryptofinancialworld.com>
2024-05-10 05:28:20 -0400

    Further notes and code for our first blockchain

--- myFirstBlockchain.hs
+++ myFirstBlockchain.hs
@@ -1,7 +1,7 @@
 data Chain =
-    GenesisBlock
-    |Block Chain Txs
-type Txs = Int
+    GenesisBlock        --constructor
+    |Block Chain Txs    --constructor
+type Txs = Int      --this makes a psuedonym for Txs which is short for "transactions". This would normally be a list of transactions, but for this example, we are just using an integer.
 
 
 
