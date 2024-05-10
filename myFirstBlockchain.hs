d1ff9ee0daae3407165780ba30af295f244da571
johnny5cfw <john@cryptofinancialworld.com>
2024-05-10 05:38:54 -0400

    adding notes to program file

--- myFirstBlockchain.hs
+++ myFirstBlockchain.hs
@@ -4,9 +4,11 @@ data Chain =
 type Txs = Int      --this makes a psuedonym for Txs which is short for "transactions". This would normally be a list of transactions, but for this example, we are just using an integer.
 
 
-
-chain1 =
-    Block GenesisBlock 2
+            --things to the left of the equals (line 8), are called "left-hand sides" that is the thing you are defining, it's and abbreviation, an identifyer that you are defining to be something, a constant in this case
+chain1 =    --the whole equation (lines 8-9), are called bindings
+    Block GenesisBlock 2            --Block applide to two arguments, GenesisBlock and 2
+    --things on the right hand side of the equals sign (line 9), are called "right-hand sides" that is the value that you are defining the left-hand side to be.
+    
 chain2 =
     Block chain1 4
 
