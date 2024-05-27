23388a084265dc65dc0424f0d30c06347635bf62
johnny5cfw <john@cryptofinancialworld.com>
2024-05-27 04:10:54 -0400

    Adding more definition to my code for review later. i am starting to understand the recursion. will study independantly about recursion. also why is the list written without syntactic sugar in the second line? can it be called recursively with [_,xs]? I should try this to see.

--- myButLast.hs
+++ myButLast.hs
@@ -1,11 +1,14 @@
 myButLast :: [a] -> a
 
 myButLast [x,_] = x
-myButLast (x:xs) = myButLast xs
+-- myButLast (x:xs) = myButLast xs   we dont care about the head (x) of the list,
+--so we make it annonamous by using _ (underscore) instead of x
+myButLast (_:xs) = myButLast xs   --then we tell the program to apply the 
+--function to the tail of the list (xs) which chops off the head of the list
 
 {-
 L = [2,3,5,7] -> 5
-BaseCaseList = [2,3] -> 2
+first we try to find the BaseCaseList = [2,3] -> 2
 
 (x:xs) lets break this down...
 x = [2] (head of the list)
@@ -15,7 +18,7 @@ xs = [3,5,7] (tail of the list)
 {- ----------------------------------------------------------------- -}
  
 -- 1st variation of the function
--}
+
 --  myButLast2 :: [b] -> b
 
 {-
